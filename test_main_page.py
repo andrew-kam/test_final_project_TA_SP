2df2daa7b737809ca652ce8ff728ccc95758cbb2
Andre <andre@mail.com>
2023-01-26 20:24:20 +0300

    rewrite with POM

--- test_main_page.py
+++ test_main_page.py
@@ -1,25 +1,15 @@
-from selenium.webdriver.common.by import By
 import pytest
-
-
-link = 'https://selenium1py.pythonanywhere.com/'
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(
-        By.CSS_SELECTOR, "#login_link"
-    )
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = 'https://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
 if __name__ == "__main__":
     pytest.main()
-
-# https://stepik.org/lesson/199980/step/6?auth=login&next=&unit=174035
 
 # pytest -v --tb=line --language=en test_main_page.py
