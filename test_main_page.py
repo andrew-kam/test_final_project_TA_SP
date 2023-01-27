d042ce8b4d4f5d874a2280b54f212f642b2d222c
Andre <andre@mail.com>
2023-01-27 10:45:48 +0300

    add def test_guest_should_see_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
 import pytest
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = 'https://selenium1py.pythonanywhere.com/'
+    link = "https://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -16,7 +17,14 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
+def test_guest_should_see_login_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+
+
 if __name__ == "__main__":
     pytest.main()
 
-# pytest -v --tb=line --language=en test_main_page.py
+# pytest -v -rpf --tb=line --language=en test_main_page.py
