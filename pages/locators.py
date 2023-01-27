1e2bf0e8cfdb3508921b4d3c174e9fbbb0f074f0
Andre <andre@mail.com>
2023-01-27 17:25:33 +0300

    add class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,16 @@ class LoginPageLocators:
         By.ID, "login_form")
     REGISTER_FORM = (
         By.ID, "register_form")
+
+
+class ProductPageLocators:
+    BASKET_LINK = (
+        By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (
+        By.CLASS_NAME, "product_main h1")
+    PRODUCT_PRICE = (
+        By.CLASS_NAME, "product_main .price_color")
+    PRODUCT_NAME_ADDED = (
+        By.CLASS_NAME, "alert-success strong")
+    PRODUCT_PRICE_ADDED = (
+        By.CLASS_NAME, "alert-info strong")
