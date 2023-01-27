8c9dc17d1e11fd1969e08b22c3c8670518c8b1d4
Andre <andre@mail.com>
2023-01-27 19:11:19 +0300

    add parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -2,8 +2,20 @@ import pytest
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link',
+                         ["https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                          "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                          "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                          "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                          "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                          "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                          "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                          pytest.param(
+                              "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                              marks=pytest.mark.xfail),
+                          "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                          "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.adding_to_basket()
@@ -16,4 +28,4 @@ def test_guest_can_add_product_to_basket(browser):
 if __name__ == "__main__":
     pytest.main()
 
-# pytest -v -s -rpf --tb=line --language=en test_product_page.py
+# pytest -v -s -rpfx --tb=line --language=en test_product_page.py
