ae1b462ff2e2b26ffe452c8caf66c394eb1c9a6c
Andre <andre@mail.com>
2023-01-28 15:09:46 +0300

    add def test_guest_cant_see_success_message_after_adding_product_to_basket,         test_guest_cant_see_success_message,         test_message_disappeared_after_adding_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -25,7 +25,32 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.check_price_added_product()
 
 
+link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+@pytest.mark.xfail(reason="expected result")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.adding_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="expected result")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.adding_to_basket()
+    page.should_success_message_disappear()
+
+
 if __name__ == "__main__":
     pytest.main()
 
-# pytest -v -s -rpfx --tb=line --language=en test_product_page.py
+# pytest -v -s -rpfxX --tb=line --language=en test_product_page.py
