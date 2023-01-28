b699d4ccbc6e260e1cf5114daaac540a425575d0
Andre <andre@mail.com>
2023-01-28 20:15:08 +0300

    add def test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 @pytest.mark.parametrize('link',
@@ -67,7 +68,18 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_product_in_basket()
+    basket_page.should_be_text_empty()
+
+
 if __name__ == "__main__":
     pytest.main()
 
-# pytest -v -s -rpfxX --tb=line --language=en test_product_page.py
+# pytest -v -s -rpfX --tb=line test_product_page.py
+# pytest -v -s -rpfX --tb=line -m "new" test_product_page.py
