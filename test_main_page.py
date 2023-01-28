1a151d224ed0a4345b15b6f1087785e612d8ebaf
Andre <andre@mail.com>
2023-01-28 22:14:13 +0300

    add class TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -4,20 +4,22 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
 
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "https://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = "https://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "https://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_should_see_login_page(browser):
@@ -41,4 +43,4 @@ if __name__ == "__main__":
     pytest.main()
 
 # pytest -v -rpf --tb=line test_main_page.py
-# pytest -v -rpf --tb=line -m "new" test_main_page.py
+# pytest -v -rpf --tb=line -m "login_guest" test_main_page.py
