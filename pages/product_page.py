8352638b781b4515a9c7720fb5445d896a756062
Andre <andre@mail.com>
2023-01-28 15:06:44 +0300

    add def should_not_be_success_message, should_success_message_disappear

--- pages/product_page.py
+++ pages/product_page.py
@@ -23,3 +23,11 @@ class ProductPage(BasePage):
             *ProductPageLocators.PRODUCT_PRICE_ADDED)
         assert product_price.text == product_price_added.text,\
             "Product price in basket isn`t correct"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_success_message_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message did not disappear"
