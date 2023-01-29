595e915098794c9f160b88ea8057a2bedc033671
Andre <andre@mail.com>
2023-01-29 16:59:12 +0300

    change links and add class TestUserAddToBasketFromProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,9 @@
+import time
 import pytest
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+from .pages.locators import Links
 
 
 @pytest.mark.parametrize('link',
@@ -22,46 +24,40 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.adding_to_basket()
     page.solve_quiz_and_get_code()
-
     page.check_name_added_product()
     page.check_price_added_product()
 
 
 @pytest.mark.xfail(reason="expected result")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, Links.PRODUCT_URL_ONE)
     page.open()
     page.adding_to_basket()
     page.should_not_be_success_message()
 
 
 def test_guest_cant_see_success_message(browser):
-    link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, Links.PRODUCT_URL_ONE)
     page.open()
     page.should_not_be_success_message()
 
 
 @pytest.mark.xfail(reason="expected result")
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, Links.PRODUCT_URL_ONE)
     page.open()
     page.adding_to_basket()
     page.should_success_message_disappear()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, Links.PRODUCT_URL_TWO)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, Links.PRODUCT_URL_TWO)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
@@ -69,8 +65,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, Links.PRODUCT_URL_TWO)
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
@@ -78,8 +73,32 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_be_text_empty()
 
 
+@pytest.mark.flaky(reruns=2)
+class TestUserAddToBasketFromProductPage:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        page = LoginPage(browser, Links.LOGIN_URL)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, Links.PRODUCT_URL_ONE)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, Links.PRODUCT_URL_ONE)
+        page.open()
+        page.adding_to_basket()
+        page.check_name_added_product()
+        page.check_price_added_product()
+
+
 if __name__ == "__main__":
     pytest.main()
 
-# pytest -v -s -rpfX --tb=line test_product_page.py
-# pytest -v -s -rpfX --tb=line -m "new" test_product_page.py
+# pytest -v -s -rpfx --tb=line test_product_page.py
