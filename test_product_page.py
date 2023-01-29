d0ddefa5501554e5364957dba1b8b1895b1e18f1
Andre <andre@mail.com>
2023-01-29 17:44:47 +0300

    add mark

--- test_product_page.py
+++ test_product_page.py
@@ -6,6 +6,33 @@ from .pages.basket_page import BasketPage
 from .pages.locators import Links
 
 
+@pytest.mark.flaky(reruns=2)
+class TestUserAddToBasketFromProductPage:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        page = LoginPage(browser, Links.LOGIN_URL)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, Links.PRODUCT_URL_ONE)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, Links.PRODUCT_URL_ONE)
+        page.open()
+        page.adding_to_basket()
+        page.check_name_added_product()
+        page.check_price_added_product()
+
+
+@pytest.mark.need_review
 @pytest.mark.parametrize('link',
                          ["https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                           "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -56,6 +83,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, Links.PRODUCT_URL_TWO)
     page.open()
@@ -64,6 +92,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, Links.PRODUCT_URL_TWO)
     page.open()
@@ -73,32 +102,5 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_be_text_empty()
 
 
-@pytest.mark.flaky(reruns=2)
-class TestUserAddToBasketFromProductPage:
-
-    @pytest.fixture(scope="function", autouse=True)
-    def setup(self, browser):
-        email = str(time.time()) + "@fakemail.org"
-        password = str(time.time())
-        page = LoginPage(browser, Links.LOGIN_URL)
-        page.open()
-        page.register_new_user(email, password)
-        page.should_be_authorized_user()
-
-    def test_user_cant_see_success_message(self, browser):
-        page = ProductPage(browser, Links.PRODUCT_URL_ONE)
-        page.open()
-        page.should_not_be_success_message()
-
-    def test_user_can_add_product_to_basket(self, browser):
-        page = ProductPage(browser, Links.PRODUCT_URL_ONE)
-        page.open()
-        page.adding_to_basket()
-        page.check_name_added_product()
-        page.check_price_added_product()
-
-
 if __name__ == "__main__":
     pytest.main()
-
-# pytest -v -s -rpfx --tb=line test_product_page.py
