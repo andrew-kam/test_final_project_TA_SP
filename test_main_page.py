c90ca57ef751c6f22255c8a5239067bc117cc3b1
Andre <andre@mail.com>
2023-01-29 16:58:27 +0300

    change links

--- test_main_page.py
+++ test_main_page.py
@@ -2,36 +2,33 @@ import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+from .pages.locators import Links
 
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage:
 
     def test_guest_can_go_to_login_page(self, browser):
-        link = "https://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
+        page = MainPage(browser, Links.MAIN_URL)
         page.open()
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "https://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
+        page = MainPage(browser, Links.MAIN_URL)
         page.open()
         page.should_be_login_link()
 
 
 def test_guest_should_see_login_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    page = LoginPage(browser, Links.LOGIN_URL)
     page.open()
     page.should_be_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, Links.MAIN_URL)
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
